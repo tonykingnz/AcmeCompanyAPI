f7f27692df15a40cb73293d649f71ce78c64a530
Tony <joseant.silva@icloud.com>
2020-09-01 18:55:43 +0000

    Created connexion system

--- app.py
+++ app.py
@@ -1,39 +1,27 @@
 import connexion
-from datetime import datetime, timedelta
 import logging
-
 from connexion import NoContent
 
-from exceptions import *
-
-from service.store_service.store import *
-
-#Store
-def list(storeAddress=None):
-    return (listStore(storeAddress))
-
-def create(store):
-    try:
-        storeId = createStore(store)
-        return (storeId, 201)
-    except ApiCustomError as e:
-        return ("Bad Request", 400)
-
-def update(store, storeId):
-    try:
-        response = updateStore(store, storeId)
-        return(response, 200)
-    except ApiCustomError as e:
-        return("Not found", 404)
-    
-#Configurations
+def listStore():
+    return NoContent
+def addStore():
+    return NoContent
+def updateStore():
+    return NoContent
+def listStoreItems():
+    return NoContent
+def addStoreItem():
+    return NoContent
+def addStoreOrder():
+    return NoContent
 logging.basicConfig(level=logging.INFO)
-app = connexion.App(__name__)
-app.add_api('swagger.yaml')
-# set the WSGI application callable to allow using uWSGI:
-# uwsgi --http :8080 -w app
-application = app.app
 
-if __name__ == '__main__':
+app = connexion.App(__name__)
+app.add_api('openapi.yaml')
+app.run(port=8080)
+
+#application = app.app
+
+#if __name__ == '__main__':
     # run our standalone gevent server
-    app.run(port=8080, server='gevent')
+    #app.run(port=8080, server='gevent')
