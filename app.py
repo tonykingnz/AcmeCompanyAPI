9506b99009cf77685f5b42a7a775e4e85b238025
Tony <joseant.silva@icloud.com>
2020-09-01 15:26:09 +0000

    Commited openapi.yaml

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import connexion
 from datetime import datetime, timedelta
 import logging
@@ -8,7 +7,6 @@ from connexion import NoContent
 from exceptions import *
 
 from service.store_service.store import *
-from service.order_service.order import *
 
 #Store
 def list(storeAddress=None):
@@ -21,13 +19,6 @@ def create(store):
     except ApiCustomError as e:
         return ("Bad Request", 400)
 
-def detail(storeId):
-    try:
-        store = detailStore(storeId)
-        return store
-    except ApiCustomError as e:
-        return ("Not found", 404)
-
 def update(store, storeId):
     try:
         response = updateStore(store, storeId)
@@ -35,55 +26,6 @@ def update(store, storeId):
     except ApiCustomError as e:
         return("Not found", 404)
     
-def remove(storeId):
-     try:
-        response = removeStore(storeId)
-        return(response, 204)
-     except ApiCustomError as e:
-        return("Not found", 404)
-
-#Order
-def listOrder(status=None):
-    return(listOrders(status))
-
-def createOrder(order):
-    try:
-        orderId = createOrders(order)
-        return (orderId, 201)
-    except ApiCustomError as e:
-        return ("Bad Request", 400)
-
-def detailOrder(orderId):
-    try:
-        order = detailOrders(orderId)
-        return order
-    except ApiCustomError as e:
-        return ("Not found", 404)
-
-def updateOrder(address, orderId):
-    try:
-        response = updateStore(address, orderId)
-        return(response, 200)
-    except ApiCustomError as e:
-        return("Not found", 404)
-
-def refund(orderId):
-    return(refundOrder(orderId))
-
-def refundItem(orderId, orderItemsID):
-    return(refundItemOrder(orderId, orderItemsID))
-
-#Payment
-def createPayment(orderId, payment):
-    try:
-        response = createPayments(orderId, payment)
-        return(response, 201)
-    except:
-        return("Bad request", 404)
-
-def paymentInformation(orderId):
-    return(paymentInformations(orderId))
-
 #Configurations
 logging.basicConfig(level=logging.INFO)
 app = connexion.App(__name__)
