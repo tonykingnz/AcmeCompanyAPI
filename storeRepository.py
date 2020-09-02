059070ef3ade7760c989cdf2a26b0bb5a0d72b1c
Tony <joseant.silva@icloud.com>
2020-09-02 00:48:43 +0000

    add sample db connection

--- storeRepository.py
+++ storeRepository.py
@@ -1,6 +1,26 @@
-#import psycopg2
+import psycopg2
 
 
 def list(name, address, pageSize, pageIndex, orderBy):
-#    return psycopg2.connect(user = "sysadmin", password = "pynative@#29", host = "127.0.0.1", port = "5432", database = "postgres_db")
-    return 'teste de integracao'
+    connection = psycopg2.connect(user = "#####", password = "*****", host = "127.0.0.1", port = "5432", database = "acme_db")
+    rs = connection.query("""
+        select 
+            * 
+        from 
+            store 
+        where 
+            (${name} is null or name like ${name}||'%')
+            and 
+            (${address} is null or name like ${address}||'%')
+            and
+            (${storeId} is null or storeId = ${storeId})
+        limit ${pageSize} + 1 offset ${pageIndex} * ${pageSize}
+    """)
+    conteudo = []
+    for item in rs:
+        conteudo[i] = rs.resolve()
+    lastPage = true
+    if conteudo.size > pageSize:
+        lastPage = false
+        conteudo.removeLastElement()
+    return PageList(conteudo, pageSize, pageIndex. lastPage)
